c7cf09f13f9977f096887f11025b8d94449c53d4
deepakts <godaddyearnings@gmail.com>
2015-02-15 00:38:01 +0530

    Add new test sample for add Method

--- src/test/java/org/math/CalculatorTest.java
+++ src/test/java/org/math/CalculatorTest.java
@@ -11,8 +11,10 @@ import org.junit.Test;
 public class CalculatorTest {
     @Test
     public  void addTest(){
+        // this is comment
         Calculator c = new Calculator();
         Assert.assertEquals(5, c.add(3, 2));
+        Assert.assertEquals(5, c.add(1, 2));
         c =null ;
     }
 
